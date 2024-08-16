a261ae64e49677a937584743bf406c97e79d1df7
Tiago Neves <tiagonevestec@gmail.com>
2024-08-16 11:06:21 -0300

    Teste - commit

--- Form32.Designer.cs
+++ Form32.Designer.cs
@@ -89,7 +89,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(336, 310);
+            this.button1.Location = new System.Drawing.Point(326, 319);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(128, 44);
